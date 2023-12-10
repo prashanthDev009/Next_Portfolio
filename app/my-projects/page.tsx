ce3d3a33c60a1f04360cfbe0508e8bffcff493de
prashanthDev009 <prashanthdev009@gmail.com>
2023-12-10 19:42:43 +0530

    New Next-Portfolio

---
+++ app/my-projects/page.tsx
@@ -0,0 +1,30 @@
+
+import ProjectCard from '@/components/ProjectCard'
+import { Projects } from '@/constants'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div
+      style={{ backgroundImage: "url(/mountains.jpg)" }}
+      className="h-screen w-screen flex items-center justify-center bg-cover bg-center">
+        <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
+
+          {Projects.map((project, index)=> (
+            <ProjectCard
+              key={index}
+              title={project.title}
+              text={project.text}
+              image={project.src}
+            />
+
+          ))}
+
+        </div>
+      
+    </div>
+    
+  )
+}
+
+export default Page
